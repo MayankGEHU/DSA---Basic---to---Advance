5032c3432d58be7aecaae840aa25b8f2d45fcdfe
MayankGEHU <minku6195@gmail.com>
2024-11-09 08:49:22 +0530

    is graph bipritie

---
+++ Graph/11-Is Graph Bipartite.cpp
@@ -0,0 +1,5 @@
+/*
+a bipartite graph is a graph whose vertices can be divided into two disjoint and 
+independent sets and, that is, every edge connects a vertex in to one in. 
+Vertex sets and are usually called the parts of the graph.
+ */
